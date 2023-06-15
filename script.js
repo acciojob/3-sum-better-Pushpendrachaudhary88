08627bb424f35a948c099edf2071af8594848066
Pushpendra Chaudhary <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-06-15 12:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+	let sum=0;
+	let a=arr[i];
+	let b=arr[i];
+	let c=arr[i];
+	for(let i=0;i<arr.length;i++){
+		if(a+b+c===target){
+			sum+=a+b+c;
+		}
+	}
+	return sum;
+	
   
 }
 
