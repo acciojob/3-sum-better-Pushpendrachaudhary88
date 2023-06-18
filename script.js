5aee2b78989a5ededf4f4af7d0bcb8038eb5e59c
Pushpendra Chaudhary <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-06-18 15:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-function threeSum(arr, target) {
+function threeSum(nums, target) {
 // write your code here
-	let sum=0;
-	let a=arr[i];
-	let b=arr[i];
-	let c=arr[i];
-	for(let i=0;i<arr.length;i++){
-		if(a+b+c===target){
-			sum+=a+b+c;
-		}
-	}
-	return sum;
-	
+// function threeSum(nums, target) {
+  // Sort the array in ascending order
+  nums.sort((a, b) => a - b);
   
-}
+  let closestSum = Infinity;
 
-module.exports = threeSum;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // We found a perfect match, so return the target sum
+        return target;
+      }
+    }
+  }
+
+  // Return the closest sum found
+  return closestSum;
+}
