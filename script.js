78e35eab06f533b493f8c9893acafe261e3fa7e9
Pushpendra Chaudhary <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-06-22 13:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-function threeSum(arr, target) {
-// write your code here
-// function threeSum(nums, target) {
+function threeSum(S, target) {
   // Sort the array in ascending order
-arr.sort((S, n) => S - n);
-  
-  let closestSum = Infinity;
+  S.sort((a, b) => a - b);
 
-  for (let i = 0; i < arr.length - 2; i++) {
+  let closestSum = Infinity; // Initialize with a large value
+
+  for (let i = 0; i < S.length - 2; i++) {
     let left = i + 1;
-    let right = arr.length - 1;
+    let right = S.length - 1;
 
     while (left < right) {
-      const sum = arr[i] + arr[left] + arr[right];
+      const sum = S[i] + S[left] + S[right];
 
-      // Check if the current sum is closer to the target
+      // Update the closest sum if the current sum is closer to the target
       if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
         closestSum = sum;
       }
 
-      if (sum < target) {
+      if (sum === target) {
+        // Found the exact target sum, return it
+        return closestSum;
+      } else if (sum < target) {
+        // Move the left pointer to increase the sum
         left++;
-      } else if (sum > target) {
-        right--;
       } else {
-        // We found a perfect match, so return the target sum
-        return target;
+        // Move the right pointer to decrease the sum
+        right--;
       }
     }
   }
 
-  // Return the closest sum found
   return closestSum;
 }
